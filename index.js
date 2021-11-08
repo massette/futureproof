6c9ecd53245bcd608e5e03e707400c6c549cb39e
massette <43896703+massette@users.noreply.github.com>
2021-11-08 18:03:53 -0500

    Update page content for water and waste

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ App.get("/trans", function(req, res, next) {
 		
 		"body": [
 			{
-				"body": "<b>Transportation is one of the more intuitive ways that we affect our carbon footprint. Beyond the simple consequences of burning gasoline, the extraction and refining of fossil fuels is inherently unsustainable because its a limitted resource. Although my household only really uses one mode of transport, we make a lot of unecessary trips.</b>"
+				"body": "<b>Transportation is one of the most intuitive ways that we shape our carbon footprint. We all understand that driving burns gasoline, and burning gasoline releases many pollutants. Beyond that, most petroleum products are on an inherrent time limit, as we are slowly but surely running out of crude oil. Despite the fact that many of us understand these facts, we treat driving as a necessary evil in the modern world, and often make very little effort to reduce mileage.</b>"
 			},
 			{},
 			{
@@ -132,7 +132,26 @@ App.get("/water", function(req, res, next) {
 		
 		"body": [
 			{
-				"body": "While water consumption doesn’t initially seem that bad, the issue is that any water we use has to then be filtered and cleaned in order to be reused. Beyond that, water use often drains effectively finite resources of water stored underground, depriving the local area of a vital resource. Because of this it is vital that we monitor the ways in which we consume water, and the sources from which it comes. My household gets water from groundwater, and the two sources that use the most water per day are the toilet and the sinks. Mr. Phillips has provided a myriad of helpful tips to reduce the amount of water the toilet uses per flush, such as putting a brick in the tank. Assuming a brick of the dimensions 8 inches x 3 inches x 2 inches, this would result in a displacement of 0.21 gallons of water, reducing the water usage per day by 2.4 gallons. This totals 876 gallons per year of water that we wouldn’t be using. The only suggestion I have to limit the amount of faucet water we use is to simply stay aware of time whenever the faucet is running. It’s easy to just turn on the faucet and waste a minute or two, but that’s an unnecessary outflow of more than a quarter gallon. By simply being conscientious, without any further effort, we could save those precious few gallons."
+				"body": "<b>Water is a strange one. When we think of the environental impact we tend to think of things that went wrong -- methan leaks, lead poisoning -- but when the system is working as intended it still puts a massive strain upon the environment. Any water that we use has to be treated before it reaches us, and then again after it leaves our home as waste water. There are many steps involved in this process that require a large amount of power. In addition, whil water is in this cycle, it is being taken away from the ecosystem it originally played a part in, and exploitation of water resources can have dire effects on the local ecosystem.</b>"
+			},
+			{},
+			{
+				"body": "Similarly to the electricity consumption, one of the biggest ways to save water consumption is to make your current appliances more efficient. This <em>can</em> mean buying new appliances like low-flow toilets or front-loading washing machines, or it can be as simple as making sure to fill the washer before every load. Get creative!"
+			},
+			{
+				"tag": "cards",
+				"cards": [
+					"<b>A lot of the appliances at our home haven't been used in a long time and need to be replaced anyways, so in that process we can try to favor water effiecient options.</b>"
+				]
+			},
+			{
+				"body": "A very simple way to limit water use is simply to stay aware of it. It's very easy to lose track of time and leave a faucet running for a minute or two while you're doing something else, but in the long run that is gallons of water that are being wasted."
+			},
+			{
+				"tag": "cards",
+				"cards": [
+					"<b>This applies to all water sources, not just sinks: hoses, showers, etc.</b>"
+				]
 			}
 		]
 	});
@@ -145,7 +164,26 @@ App.get("/waste", function(req, res, next) {
 		
 		"body": [
 			{
-				"body": "Examining what we throw away shows us a lot about our impact on the environment. Not just what we contribute to landfills and recycling plants, but also what products we consume. Because our continued patronage of unsustainable products encourages industries to continue making them, in a way we are responsible not just for the cost of disposal, but also for the cost of creation. For many products, both of these processes are costly and rely on limited resources. The products that made up the majority of our garbage over the period that we measured were paper and plastic. We think of both of these as highly disposable products despite the fact that both take a long time to break down. Plastic is infamous for its permanence: it doesn’t break down naturally, and we have no real way to dispose of it aside from recycling it into new plastics. The recycling process is expensive and can produce toxic chemicals. Over time it also degrades the quality of the recycled plastic, making it further unsustainable, making the only real way to dispose of plastic to stop using it all together. Paper on the other hand is not much better. Not only does it take an enormous amount of water and energy to create paper, but it deprives us of another crucial resource: trees. Despite this, 42% of all global wood harvest goes towards paper. We cannot afford to treat either of these materials as disposable in the face of their impending consequences. While some of the waste directly from “disposable” products that we have bought, the vast majority of it enters our house in a way that is difficult to avoid: food wrapping. The first obvious step is to stop the little bit that we are directly contributing. No more solo cups, or paper towels, or anything else that could be replaced with a more permanent alternative. The simplest solution to the food packaging issue is to buy in bulk or shop somewhere that lets you bring your own containers. While I know that the Ingles in Franklin and Clayton sell nuts and things in bulk, I'm not sure that this is a feasible alternative for all of the products that we consume. Ultimately it is the best first step that I can think of, short of making everything we ever use from scratch."
+				"body": "<b>Waste acts as sort of a catch all for evverything else we consume. By looking at our garbage, we can see not only how we contribute to landfills and recylcing plants, but also what products we use regularly. By examining the carbon footprint of these products, we can get an idea of the less direct pieces.</b>"
+			},
+			{},
+			{
+				"body": "The first thing to do to reduce waste is to stop consuming products made solely to be thrown away. There are many disposable products in our life that could be replaced by a more permanent solution at a slight inconvenience."
+			},
+			{
+				"tag": "cards",
+				"cards": [
+					"<b>This includes plastic cups, plastic utensils, paper plates, and paper towels..</b>"
+				]
+			},
+			{
+				"body": "However, many of the products that we use are stored in disposable packaging. Buying in bulk limits the amount of packaging that is needed, but beyond that there is often no good alternative."
+			},
+			{
+				"tag": "cards",
+				"cards": [
+					"<b>Buying in bulk and only purchasing whatever prepackaged you actually need to buy prepackaged is the most you're really able to do for this.</b>"
+				]
 			}
 		]
 	});
